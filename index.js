525fed17d8431bf24f36cfc7959be37c5e98402d
Elijah Prince <elijahprince73@gmail.com>
2019-09-19 21:42:54 -0700

    can delete a link and update

--- index.js
+++ index.js
@@ -1,38 +1,48 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`;
-
 let links = [{
   id: 'link-0',
   url: 'www.howtographql.com',
   description: 'Fullstack tutorial for GraphQL'
 }]
 
+let idCount = links.length
+
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
+    link: (parent, args) => links.find((link) => link.id === args.id)
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      }
+
+      links.push(link)
+      return link
+    },
+    updateLink: (parent, args) => {
+      const linkIndex = links.findIndex((link) => link.id === args.id)
+      links[linkIndex] =  {...links[linkIndex], ...args };
+      
+      return links[linkIndex];
+    },
+    deleteLink: (parent, args) => {
+      const linkIndex = links.findIndex(link => link.id === args.id);
+      const link = links[linkIndex]
+      links.splice(linkIndex, 1)
+
+      return link;
+    }
   }
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 })
 
